00b37431ea838fbf6798e0b6d40b8a61ef00924e
dai <sugerbig15@gmail.com>
2024-02-28 14:05:58 +0900

    nodeバージョン統一

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -4,9 +4,7 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '登録機能' do
     context 'タスクを登録した場合' do
       it '登録したタスクが表示される' do
-        task = FactoryBot.create(:task, title: '書類作成', content: '企画書を作成する。')
         visit new_task_path
-        FactoryBot.build(:task)
         fill_in 'task[title]', with: task.title
         fill_in 'task[content]', with: task.content
         click_on 'Create Task'
