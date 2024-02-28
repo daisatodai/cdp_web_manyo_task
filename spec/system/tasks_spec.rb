ba09ea32360714b020d4cabc97d86410f7194214
dai <sugerbig15@gmail.com>
2024-02-28 11:18:52 +0900

    system spec完了、課題提出

---
+++ spec/system/tasks_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.describe 'タスク管理機能', type: :system do
+  describe '登録機能' do
+    context 'タスクを登録した場合' do
+      it '登録したタスクが表示される' do
+        task = FactoryBot.create(:task, title: '書類作成', content: '企画書を作成する。')
+        visit new_task_path
+        FactoryBot.build(:task)
+        fill_in 'task[title]', with: task.title
+        fill_in 'task[content]', with: task.content
+        click_on 'Create Task'
+        expect(page).to have_content '書類作成'
+      end
+    end
+  end
+
+  describe '一覧表示機能' do
+    context '一覧画面に遷移した場合' do
+      it '登録済みのタスク一覧が表示される' do
+        # テストで使用するためのタスクを登録
+        # Task.create!(title: '書類作成', content: '企画書を作成する。')
+        FactoryBot.create(:task)
+        # タスク一覧画面に遷移
+        visit tasks_path
+        # visit（遷移）したpage（この場合、タスク一覧画面）に"書類作成"という文字列が、have_content（含まれていること）をexpect（確認・期待）する
+        expect(page).to have_content '書類作成'
+        # expectの結果が「真」であれば成功、「偽」であれば失敗としてテスト結果が出力される
+      end
+    end
+  end
+
+  describe '詳細表示機能' do
+    context '任意のタスク詳細画面に遷移した場合' do
+      it 'そのタスクの内容が表示される' do
+        task = FactoryBot.create(:task)
+        visit task_path(task)
+        expect(page).to have_content '企画書を作成する。'
+      end
+    end
+  end
+end
