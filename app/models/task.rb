417f3ff61ee1d7652e8e4d2e21efba87a5d55ae9
dai <sugerbig15@gmail.com>
2024-03-05 11:07:39 +0900

    scope追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,5 @@ class Task < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
   paginates_per 10
+  scope :created_at, -> { order(created_at: :desc) }
 end
